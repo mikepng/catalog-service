7ec3d0639662b778c5fddaa3f7905112fc202451
mikepng <asimakispng@gmail.com>
2023-09-26 21:29:32 +0100

    first commit

---
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,34 @@
+//package com.polarbookshop.catalogservice;
+//
+//import com.polarbookshop.catalogservice.domain.Book;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.web.reactive.server.WebTestClient;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//class CatalogServiceApplicationTests {
+//
+//	@Autowired
+//	private WebTestClient webTestClient;
+//
+//	@Test
+//	void whenPostRequestThenBookCreated() {
+//		var expectedBook = new Book("123456789012", "Title", "Author", 9.90);
+//
+//		webTestClient
+//				.post()
+//				.uri("/books")
+//				.bodyValue(expectedBook)
+//				.exchange()
+////				.expectStatus().is2xxSuccessful()
+//				.expectBody(Book.class)
+//				.value( actualBook -> {
+//					assertThat(actualBook).isNotNull();
+//					assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+//				});
+//	}
+//
+//}
